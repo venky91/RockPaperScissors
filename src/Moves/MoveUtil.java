7bbe16e4bbf5a6717e0badad00b373aea0da64c9
venky91 <venksundram@gmail.com>
2016-02-17 20:23:48 -0500

    Added util class to instantiate moves to be used by computer and human

---
+++ src/Moves/MoveUtil.java
@@ -0,0 +1,9 @@
+package Moves;
+
+public class MoveUtil {
+	
+	public static Rock rock = new Rock();
+	public static Paper paper = new Paper();
+	public static Scissors scissors = new Scissors();
+	
+}
