a2b93e4093a5abd29f361e2a1db83bc0bc1bc9fa
venky91 <venksundram@gmail.com>
2016-02-17 21:18:13 -0500

    Implemented move set and refactored

--- src/Players/Human.java
+++ src/Players/Human.java
@@ -1,16 +1,35 @@
 package Players;
+import java.util.Scanner;
+
 import Moves.Move;
+import Moves.MoveUtil;
 
 
 public class Human {
 
-	private Move move;
-	
-	public void setMove(Move move) {
-		this.move = move;
+	private Move currentMove;
+	Scanner sc = new Scanner(System.in);
+		
+	public Move getCurrentMove() {
+		return currentMove;
 	}
 	
-	public Move getMove() {
-		return move;
+	public boolean move() {
+		boolean ret = false;
+		String input = sc.nextLine();
+		if (input != null && input.length() == 1) {
+			char c = input.charAt(0);
+			if (c == 'r') {
+				currentMove = MoveUtil.rock;
+				ret = true;
+			} else if (c == 'p') {
+				currentMove = MoveUtil.paper;
+				ret = true;
+			} else if (c == 's') {
+				currentMove = MoveUtil.scissors;
+				ret = true;
+			}
+		}
+		return ret;
 	}
 }
