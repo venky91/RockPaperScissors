876f5fb97c944b2cf2b7075c1f6bfe6866ede5d0
venky91 <venksundram@gmail.com>
2016-02-17 20:26:01 -0500

    Added currentMove attribute

--- src/Players/Computer.java
+++ src/Players/Computer.java
@@ -1,8 +1,21 @@
 package Players;
 
+import Moves.Move;
+
 public abstract class Computer {
 
+	private Move currentMove;
 	public abstract void move();
 	public abstract void processEndOfRound();
+	
+	public void setCurrentMove(Move move) {
+		this.currentMove = move;
+	}
+	
+	public Move getCurrentMove() {
+		return currentMove;
+	}
+	
+	
 	
 }
